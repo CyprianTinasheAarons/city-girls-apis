84d9f6640756cb6287726ba536eb94141a0d0dd8
Fraud47 <sojandem@gmail.com>
2022-01-10 10:29:24 +0200

    get

--- routes/services.routes.js
+++ routes/services.routes.js
@@ -10,7 +10,7 @@ module.exports = (app) => {
   router.get("/", services.findAll);
   router.get("/:category", services.filter);
   router.get("/location/:location", services.filterLocation);
-  router.post("/user/:id", services.findOne);
+  router.get("/user/:id", services.findOne);
 
 
   router.delete("/:id", [authJwt.verifyToken],services.delete);
